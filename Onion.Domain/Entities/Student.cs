ef77e4588abaa856c46318843b97687f84e85b3f
Jan Bryński <s16839@pjwstk.edu.pl>
2020-03-26 17:18:12 +0100

    initial commit

---
+++ Onion.Domain/Entities/Student.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Onion.Domain.Entities
+{
+    public class Student
+    {
+        public int IdStudent { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ Onion.Infrastructure.SqlSeverService/Services/SqlServerService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+
+
+namespace Onion.Infrastructure.SqlSeverService.Services
+
+{
+    public class SqlServerService : IStudentDbService
+    {
+        private List<Student> students = new List<Student>();
+        private string connectionString = "Data Source=db-mssql;Initial Catalog=s16839;Integrated Security=True";
+
+        public bool EnrollStudent(Student newStudent, int semestr)
+        {
+            int queryResult = -1;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand($"insert into Student (Id, FirstName, LastName) values ({newStudent.IdStudent},{newStudent.FirstName}),{newStudent.LastName}", connection))
+                {
+                    
+                    queryResult = command.ExecuteNonQuery();
+                    
+                }
+            }
+
+            if (queryResult == 1)
+                return true;
+
+            return false;
+        }
+
+        public IEnumerable<Student> GetStudents() {
+            DataTable dataReceived = new DataTable();
+            using (SqlConnection connection = new SqlConnection(connectionString)) {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand("select * from Students", connection))
+                {
+                    dataTable.Load(command.ExecuteReader());
+                }
+            }
+            foreach (DataRow row in dataTable.Rows)
+            {
+                students.Add(new Student()
+                {
+                    IdStudent = Convert.ToInt32(row[0]),
+                    FirstName = row[1].ToString(),
+                    LastName = row[2].ToString()
+                });
+            }
+            return students;
+        }
+    }
+}
+
